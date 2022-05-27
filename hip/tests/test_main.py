548a7398962a2d82018aca9385480a0a3bd17fd8
Reuben Brasher <rkbrasher@gmail.com>
2022-05-27 10:34:21 -0700

    Update expected probabilities for test TestMain.test_rbd_189_binds

--- hip/tests/test_main.py
+++ hip/tests/test_main.py
@@ -14,10 +14,25 @@ class TestMain(unittest.TestCase):
 
     def test_rbd_189_binds(self):
         embedding, final_scores = mn.main("./hip/tests/testdata/rbd_189_config.yml")
+
+        # verify that embeddings are as expected
         expected = np.load("./hip/tests/fixture/rbd_189_embedding.npy")
         actual = embedding
         self.assertAlmostEqual(0, norm(expected - actual))
 
-        expected = np.load("./hip/tests/fixture/rbd_189_final_scores_binds_prob.npy")
-        actual = final_scores.Binds_prob.values
+        # verify that highest probability has expected index
+        probs = final_scores.Binds_prob.values
+        expected = 183
+        actual = np.argmax(probs)
         self.assertAlmostEqual(0, norm(expected - actual))
+
+        # verify that probability distribution is as expected
+        expected = np.load("./hip/tests/fixture/rbd_189_final_scores_binds_prob.npy")
+        actual = probs
+        self.assertAlmostEqual(0, norm(expected - actual))
+    #
+    # def test_model_save_locations(self):
+    #     self.fail("incomplete")
+    #
+    # def test_restore_model(self):
+    #     self.fail("incomplete")
