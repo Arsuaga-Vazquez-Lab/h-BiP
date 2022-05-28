c7202050afdca9604046e0e1172548534656629d
Reuben Brasher <rkbrasher@gmail.com>
2022-05-28 12:33:31 -0700

    Test that logistic regression makes same predictions after reloading

--- hip/tests/test_main.py
+++ hip/tests/test_main.py
@@ -1,8 +1,30 @@
 import unittest
 import numpy as np
 from numpy.linalg import norm
+import os
+import pickle
 
 import main as mn
+
+
+def base_model_path(name):
+    return os.path.join("./models", name)
+
+
+def model_files(name):
+    return [
+        os.path.join(base_model_path(name), filename)
+        for filename in "LR85.pkl	mat_vect.pkl	mean_vec.pkl	scale.pkl	vocab.pkl".split()
+    ]
+
+
+def unlink_model(name):
+    path = base_model_path(name)
+    if not os.path.isdir(path):
+        return
+    for filename in model_files(name):
+        os.unlink(filename)
+    os.rmdir(path)
 
 
 class TestMain(unittest.TestCase):
@@ -10,7 +32,7 @@ class TestMain(unittest.TestCase):
         pass
 
     def tearDown(self):
-        pass
+        unlink_model("rbd_189")
 
     def test_rbd_189_binds(self):
         embedding, final_scores = mn.main("./hip/tests/testdata/rbd_189_config.yml")
@@ -30,9 +52,24 @@ class TestMain(unittest.TestCase):
         expected = np.load("./hip/tests/fixture/rbd_189_final_scores_binds_prob.npy")
         actual = probs
         self.assertAlmostEqual(0, norm(expected - actual))
-    #
-    # def test_model_save_locations(self):
-    #     self.fail("incomplete")
-    #
-    # def test_restore_model(self):
-    #     self.fail("incomplete")
+
+    def test_model_save_locations(self):
+        # verify that model is saved with expected file names in expected directory
+        mn.main("./hip/tests/testdata/rbd_189_config.yml", save_model=True)
+        base_path = base_model_path("rbd_189")
+        filesnames = model_files("rbd_189")
+        self.assertTrue(os.path.isdir(base_path))
+        for filename in filesnames:
+            self.assertTrue(os.path.exists(filename), msg=filename)
+
+    def test_restore_model(self):
+        # train and save model
+        mn.main("./hip/tests/testdata/rbd_189_config.yml", save_model=True)
+
+        # reload model
+        with open("./models/rbd_189/LR85.pkl", "rb") as f:
+            LR85 = pickle.load(f)
+        seq = np.load("./hip/tests/fixture/rbd_189_Xall_norm.npy")
+        actual = mn.predict(LR85, seq)
+        expected = np.load("./hip/tests/fixture/rbd_189_final_scores_binds_prob.npy")
+        self.assertAlmostEqual(0, norm(expected - actual))
