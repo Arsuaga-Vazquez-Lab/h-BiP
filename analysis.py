d5c67baa29749f7a6851e592b85f3279ddbd648e
Georgina Gonzalez <gina@Georginas-MacBook-Pro.local>
2023-04-14 14:00:40 -0700

    Updated binding with Khaledian et al. 2022

--- analysis.py
+++ analysis.py
@@ -7,14 +7,16 @@ def main(add_max=False):
     pd.options.display.width = 100
     pd.options.display.max_columns = 10
 
-    path = "hbip_scores/alpha_beta_scores.csv"
+    # path = "hbip_scores/alpha_beta_scores.csv"
+    path = "hbip_scores/alpha_beta_new_scores.csv"
     if add_max:
         df_max = add_max_identity(path)
     else:
         df_max = pd.read_csv(path)
     print(df_max.head())
-    plot_correlation(df_max, fig_name="ab_correlation_annotated", size="full")
+    # plot_correlation(df_max, fig_name="ab_correlation_annotated", size="full")
+    plot_correlation(df_max, fig_name="ab_correlation_annotated, updated binding", size="full")
 
 
 if __name__ == "__main__":
-    main()
+    main(add_max=True)
