3a889a628b9a3f57570dadd3447de0e50824dfb9
Georgina Gonzalez <gina@Georginas-MacBook-Pro.local>
2023-04-14 18:42:28 -0700

    Updated bind and improved correlation plot

--- analysis.py
+++ analysis.py
@@ -1,22 +1,38 @@
 import pandas as pd
+import sys
 
 from hbip.utils.analysis_utils import plot_correlation, add_max_identity
 
 
-def main(add_max=False):
+def main(scores_path, title=None, add_max=False):
     pd.options.display.width = 100
     pd.options.display.max_columns = 10
 
     # path = "hbip_scores/alpha_beta_scores.csv"
-    path = "hbip_scores/alpha_beta_new_scores.csv"
     if add_max:
-        df_max = add_max_identity(path)
+        df_max = add_max_identity(scores_path)
     else:
-        df_max = pd.read_csv(path)
+        df_max = pd.read_csv(scores_path)
     print(df_max.head())
     # plot_correlation(df_max, fig_name="ab_correlation_annotated", size="full")
-    plot_correlation(df_max, fig_name="ab_correlation_annotated, updated binding", size="full")
+    plot_correlation(df_max, fig_name=title, size="full", print_correlation=True)
 
 
 if __name__ == "__main__":
-    main(add_max=True)
+    
+    """
+    Usage CLI: python3 analysis.py
+    Usage CLI: python3 analysis.py path_to_scores title
+    Usage CLI: python3 hbip_predict.py hbip_scores/alpha_beta_new_scores.csv "ab_correlation_annotated, updated binding"
+    If no parameters are provided it will generate the scatterplot from alpha_beta_scores.csv
+    """
+
+    if len(sys.argv) == 1 :
+        print('first if')
+        path = "hbip_scores/alpha_beta_scores.csv"
+        title= "ab_correlation_annotated"
+    elif len(sys.argv) > 1:
+        path = sys.argv[1]
+        title= sys.argv[2]
+
+    main(scores_path = path, title=title, add_max=False )
